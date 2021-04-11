9e07869f05ec86417bf0b1a0e47137a7b4440a49
chaitumaroju <55808712+chaitumaroju@users.noreply.github.com>
2021-04-10 22:33:25 -0500

    get table names and cols works

--- backend/server/routes.py
+++ backend/server/routes.py
@@ -1,9 +1,7 @@
 from flask import request, jsonify
 from server import app
 from server import db
-
 from server import database as dbase
-
 
 
 @app.route('/insert')
@@ -17,18 +15,47 @@ def update_route():
     return render_template("index.html", data = data)
 
 
-@app.route('/updateDatabase', methods=['GET'])
-def hello_world():
-    arg2 = request.args.get('cow')
-    fun1()
-    return str(arg2)
-
 @app.route('/getTableData', methods=['GET'])
 def getTableData():
     table = request.args.get('table')
     result = dbase.get_table_data(table)
     results = [dict(row) for row in result] 
     return jsonify({'results': results})
+
+# @app.route('/updateDatabase', methods=['GET'])
+# def hello_world():
+#     arg2 = request.args.get('cow')
+#     fun1()
+#     return str(arg2)
+
+# @app.route('/delete', methods=['GET'])
+# def delete():
+#     table = request.args.get('table')
+#     id = request.args.get('id')
+#     db.delete(table, int(id))
+
+@app.route('/getTableColumns', methods=['GET'])
+def getColumnsFromTable():
+    arg1 = str(request.args.get('tableName'))
+    conn = db.connect()
+    query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'covid_tracker' AND TABLE_NAME = '{}';".format(arg1)
+    query_results = conn.execute(query)
+    conn.close()
+
+    results = [dict(row) for row in query_results]
+    result_dict = {'results': results}
+    return jsonify(result_dict)
+
+@app.route('/getTableNames', methods=['GET'])
+def getTableNames():
+    conn = db.connect()
+    query = "SELECT Table_name as TableName from information_schema.tables where table_schema = 'covid_tracker';"
+    query_results = conn.execute(query)
+    conn.close()
+
+    results = [dict(row) for row in query_results]
+    result_dict = {'results': results}
+    return jsonify(result_dict)
 
 @app.route('/delete', methods=['GET'])
 def delete():
