d9edf82984e77e5ccada9bd8d79bda11a403f3f8
Brandon Wong <brandonjwong@gmail.com>
2021-04-10 19:20:08 -0500

    backend formatting

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import database as db
 app = Flask(__name__)
 
+@app.route('/insert')
+def insert_route():
+    data = db.fetch_data()
+    return render_template("index.html", data = data)
+
+@app.route('/update')
+def update_route():
+    data = db.fetch_data()
+    return render_template("index.html", data = data)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    data = db.fetch_data()
+    return render_template("index.html", data = data)
